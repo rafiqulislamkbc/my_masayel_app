7fc53e6fbb82628492acf52521d16d3e2b8026ea
Rafiqul Islam <rafiqulislamkbc@gmail.com>
2026-08-13 18:30:21 +0600

    Update desktop window title / hide title bar

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -27,7 +27,7 @@ int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
   FlutterWindow window(project);
   Win32Window::Point origin(10, 10);
   Win32Window::Size size(1280, 720);
-  if (!window.Create(L"masayel_app", origin, size)) {
+  if (!window.Create(L"আপনি যা জানতে চেয়েছেন", origin, size)) {
     return EXIT_FAILURE;
   }
   window.SetQuitOnClose(true);
